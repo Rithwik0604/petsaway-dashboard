91f2c226afdc960635654120a87e11b3b43ed4ff
Rithwik <rithwik@delhipeople.com>
2026-02-01 20:43:49 +0000

    Implement authentication and client management API; add middleware and setup functions

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"petsaway/internal/database"
+	"petsaway/internal/server"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	// setup database
+	database.SetupDatabase()
+
+	// setup server
+	server.SetupServer()
+}
